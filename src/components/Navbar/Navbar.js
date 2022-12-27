75c02d85a8225d57d036ae73354387a1a39beb19
Ignas Vaitkus <ignas.vaitkus7@gmail.com>
2022-12-27 14:49:38 +0200

    Responsive Navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -9,6 +9,12 @@ const Navbar = () => (
         <li>Lorem Ipsum</li>
         <li>Lorem Ipsum</li>
       </ul>
+      <input class="checkbox" type="checkbox" name="" id="" />
+      <div class="hamburger-lines">
+        <span class="line line1"></span>
+        <span class="line line2"></span>
+        <span class="line line3"></span>
+      </div>
     </nav>
   </header>
 );

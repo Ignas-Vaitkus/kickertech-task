9aef770929fb937922dafe3e0ebf73e2e9e788c0
Ignas Vaitkus <ignas.vaitkus7@gmail.com>
2022-12-27 14:24:38 +0200

    Full-width navbar

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Navbar.css";
+
+const Navbar = () => (
+  <header className="Navbar">
+    <nav>
+      <ul>
+        <li>Lorem Ipsum</li>
+        <li>Lorem Ipsum</li>
+        <li>Lorem Ipsum</li>
+      </ul>
+    </nav>
+  </header>
+);
+
+export default Navbar;

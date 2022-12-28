3678b5239afdfaf8b5302660337cd3222bcaa859
Ignas Vaitkus <ignas.vaitkus7@gmail.com>
2022-12-28 09:22:33 +0200

    Moved header element and rendering post components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
+import Post from "./components/Post/Post";
 
 function App() {
+  const posts = [
+    {
+      title: "Lorem ipsum dolor sit amet, consectetur",
+      author: "Lorem ipsum",
+      about: "Lorem ipsum dolor sit",
+    },
+    {
+      title: "Lorem ipsum dolor sit amet, consectetur",
+      author: "Lorem ipsum",
+      about: "Lorem ipsum dolor sit",
+    },
+    {
+      title: "Lorem ipsum dolor sit amet, consectetur",
+      author: "Lorem ipsum",
+      about: "Lorem ipsum dolor sit",
+    },
+  ];
+
   return (
     <div className="App">
-      <Navbar />
+      <header>
+        <Navbar />
+      </header>
+      <main>
+        {posts.map((post) => (
+          <Post {...post} />
+        ))}
+      </main>
     </div>
   );
 }
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -2,21 +2,19 @@ import React from "react";
 import "./Navbar.css";
 
 const Navbar = () => (
-  <header className="Navbar">
-    <nav>
-      <input class="checkbox" type="checkbox" />
-      <div class="hamburger-lines">
-        <span class="line line1"></span>
-        <span class="line line2"></span>
-        <span class="line line3"></span>
-      </div>
-      <ul className="menu-items">
-        <li>Lorem Ipsum</li>
-        <li>Lorem Ipsum</li>
-        <li>Lorem Ipsum</li>
-      </ul>
-    </nav>
-  </header>
+  <nav className="Navbar">
+    <input class="checkbox" type="checkbox" />
+    <div class="hamburger-lines">
+      <span class="line line1"></span>
+      <span class="line line2"></span>
+      <span class="line line3"></span>
+    </div>
+    <ul className="menu-items">
+      <li>Lorem Ipsum</li>
+      <li>Lorem Ipsum</li>
+      <li>Lorem Ipsum</li>
+    </ul>
+  </nav>
 );
 
 export default Navbar;

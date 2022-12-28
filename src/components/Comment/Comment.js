68bfcf63d700d5fe5db38f835c5df38d8f635e51
Ignas Vaitkus <ignas.vaitkus7@gmail.com>
2022-12-28 09:21:55 +0200

    Created comment component

---
+++ src/components/Comment/Comment.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './Comment.css';
+
+const Comment = () => (
+  <div className="Comment">
+    Comment Component
+  </div>
+);
+
+Comment.propTypes = {};
+
+Comment.defaultProps = {};
+
+export default Comment;

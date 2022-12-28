a16ab0ec4fad49761310f998d4a25e78f7ae1a09
Ignas Vaitkus <ignas.vaitkus7@gmail.com>
2022-12-28 12:24:33 +0200

    PropTypes set

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -8,8 +8,16 @@ const Post = () => {
   return <div className="Post">Post Component</div>;
 };
 
-Post.propTypes = {};
+Post.propTypes = {
+  title: PropTypes.string,
+  author: PropTypes.string,
+  about: PropTypes.string,
+};
 
-Post.defaultProps = {};
+Post.defaultProps = {
+  title: "Lorem ipsum dolor sit amet, consectetur",
+  author: "Lorem ipsum",
+  about: "Lorem ipsum dolor sit",
+};
 
 export default Post;
